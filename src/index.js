90d319ee3602c177ed36ea4cbb73ccf5a8a60bd2
coll0256 <marion.collard@etudiant.univ-reims.fr>
2025-01-15 18:47:55 +0100

    add end scene to game

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Level1 from "./Level1";
 import Level2 from "./Level2";
 import Level3 from "./Level3";
 import Title from "./Title";
+import End from "./End";
 
 /**
  * Configuration Phaser.
@@ -14,4 +15,4 @@ game.scene.add("Title", Title, true);
 game.scene.add("Level1", Level1);
 game.scene.add("Level2", Level2);
 game.scene.add("Level3", Level3);
-
+game.scene.add("End", End);
