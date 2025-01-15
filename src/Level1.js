c1ad5a7c3af209084ccc74142ed8c0921c58e560
coll0256 <marion.collard@etudiant.univ-reims.fr>
2025-01-15 16:47:56 +0100

    update level1 with chicken

--- src/Level1.js
+++ src/Level1.js
@@ -1,7 +1,7 @@
-import StoneGroup from "./StoneGroup";
+import SandGroup from "./SandGroup";
+
 import Player from "./Player";
-// eslint-disable-next-line no-unused-vars -- use later
-import TileGroup from "./TileGroup";
+import Chicken from "./Chicken";
 
 import Phaser from "phaser";
 
@@ -13,19 +13,21 @@ export default class Level1 extends Phaser.Scene {
     super({ key: "Level1" });
     this.stoneGroup = null; // Groupe de pierres
     this.player = null; // Instance du joueur
+    this.chicken = null; // Instance du poulet
     this.keys = {}; // Stocke les touches
     this.background = null; // Arrière-plan
   }
 
   preload() {
-    StoneGroup.preload(this); // Précharge l'image pour les pierres.
+    SandGroup.preload(this); // Précharge l'image pour les pierres.
     Player.preload(this); // Charge l'image du joueur
+    Chicken.preload(this); // Charge l'image du poulet
     this.load.image("tropicalBackground", "img/tropic.webp");
   }
 
   create() {
     // Dimensions du niveau
-    const levelWidth = 23 * 64; // 24 colonnes de 64 pixels (largeur totale du niveau)
+    const levelWidth = 23 * 64 * 4; // 24 colonnes de 64 pixels (largeur totale du niveau)
     const levelHeight = 9 * 64; // 9 lignes de 64 pixels (hauteur totale du niveau)
 
     this.background = this.add
@@ -44,25 +46,27 @@ export default class Level1 extends Phaser.Scene {
     // Définir les limites du monde physique
     this.physics.world.setBounds(0, 0, levelWidth, levelHeight);
 
-    // Configurer les blocs de pierre
-    this.stoneGroup = new StoneGroup(this);
-    this.stoneGroup.addTiles(0, 8, 21, 1); // Sol de pierres (col 0-6, ligne 8)
+    // Configurer les blocs de sable
+    this.stoneGroup = new SandGroup(this);
+    this.stoneGroup.addTiles(0, 9, 100, 1); // Sol de pierres (col 0-99, ligne 9)
+    this.stoneGroup.addTiles(0, 8, 100, 1); // Sol de pierres (col 0-99, ligne 8)
 
     // Configurer la caméra
     this.cameras.main.setBounds(0, 0, levelWidth, levelHeight); // Limites de la caméra
     this.cameras.main.setZoom(1);
 
     // Ajouter un joueur
-    this.player = new Player(this, 1, 7); // Position initiale : (1, 7), ajusté pour être sur la plateforme
+    this.player = new Player(this, 1, 4); // Position initiale : (1, 4), ajusté pour être sur la plateforme
     this.physics.add.collider(this.player, this.stoneGroup); // Gestion des collisions
+
+    // Ajouter un poulet
+    this.chicken = new Chicken(this, 10, 4); // Position initiale : (1, 10), ajusté pour être sur la plateforme
+    this.physics.add.collider(this.chicken, this.stoneGroup); // Gestion des collisions
 
     // Faire suivre la caméra au joueur
     this.cameras.main.startFollow(this.player);
 
     // Configuration des touches
-    this.keys.left = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.LEFT,
-    );
     this.keys.right = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.RIGHT,
     );
@@ -78,24 +82,16 @@ export default class Level1 extends Phaser.Scene {
   }
 
   #handleInput() {
-    this.keys.left.on("down", () => this.#handleMove());
-    this.keys.left.on("up", () => this.#handleMove());
     this.keys.right.on("down", () => this.#handleMove());
     this.keys.right.on("up", () => this.#handleMove());
   }
 
   #handleMove() {
-    const left = this.keys.left.isDown;
     const right = this.keys.right.isDown;
 
-    if (left && right) {
-      this.player.halt(); // Les deux touches pressées
-    } else if (left) {
-      this.player.moveLeft(); // Se déplace à gauche
-    } else if (right) {
+    if (right) {
       this.player.moveRight(); // Se déplace à droite
-    } else {
-      this.player.halt(); // Arrêt
+      this.chicken.moveRight(); // Le poulet se déplace à droite
     }
   }
 }
