5465eaeabeb5c94c74e2419af2a11a931ccf5b0b
coll0256 <marion.collard@etudiant.univ-reims.fr>
2025-01-15 15:34:23 +0100

    add tilegroup

---
+++ src/TileGroup.js
@@ -0,0 +1,27 @@
+import Phaser from "phaser";
+
+/**
+ * Groupe de tuiles générique.
+ */
+export default class TileGroup extends Phaser.Physics.Arcade.StaticGroup {
+  constructor(scene, tileName, tileWidth, tileHeight) {
+    super(scene.physics.world, scene);
+    this.tileName = tileName;
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
+  }
+
+  addTiles(x, y, width = 1, height = 1) {
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const tile = this.create(
+          (x + col) * this.tileWidth,
+          (y + row) * this.tileHeight,
+          this.tileName,
+        ).setOrigin(0, 0);
+        tile.body.updateFromGameObject();
+      }
+    }
+    return this;
+  }
+}
