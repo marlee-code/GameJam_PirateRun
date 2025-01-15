a9ee12ba4dfbd20918feee872d2f7f4bdc080878
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-01-15 18:29:50 +0100

    Ajout niveau 3

--- src/Level3.js
+++ src/Level3.js
@@ -1,5 +1,4 @@
-import GrassGroup from "./GrassGroup";
-import DirtGroup from "./DirtGroup";
+import StoneGroup from "./StoneGroup";
 
 import Pirate from "./Pirate";
 import Chicken from "./Chicken";
@@ -9,11 +8,10 @@ import Phaser from "phaser";
 /**
  * Scène du deuxième niveau.
  */
-export default class Level2 extends Phaser.Scene {
+export default class Level3 extends Phaser.Scene {
   constructor() {
-    super({ key: "Level2" });
-    this.grassGroup = null; // Groupe de terres
-    this.dirtGroup = null; // Groupe de terres
+    super({ key: "Level3" });
+    this.stoneGroup = null; // Groupe de pierres
     this.player = null; // Instance du joueur
     this.chicken = null; // Instance du poulet
     this.keys = {}; // Stocke les touches
@@ -21,11 +19,10 @@ export default class Level2 extends Phaser.Scene {
   }
 
   preload() {
-    GrassGroup.preload(this); // Précharge l'image pour la terre.
-    DirtGroup.preload(this); // Précharge l'image pour l'herbe.
+    StoneGroup.preload(this);
     Pirate.preload(this); // Charge l'image du joueur
     Chicken.preload(this); // Charge l'image du poulet
-    this.load.image("tropicalForestBackground", "img/tropical_forest.webp");
+    this.load.image("castle_wall", "img/castlewall.png");
     this.load.image("wood", "img/wood.png");
   }
 
@@ -35,7 +32,7 @@ export default class Level2 extends Phaser.Scene {
     const levelHeight = 9 * 64; // 9 lignes de 64 pixels (hauteur totale du niveau)
 
     this.background = this.add
-      .tileSprite(0, 0, 8000, 600, "tropicalForestBackground")
+      .tileSprite(0, 0, 8000, 600, "castle_wall")
       .setOrigin(0, 0);
     // La largeur (8000) peut être adaptée à la taille totale du monde.
 
@@ -43,7 +40,7 @@ export default class Level2 extends Phaser.Scene {
     this.add.image(0, 10, "wood").setOrigin(0, 0).setScrollFactor(0);
 
     this.add
-      .text(10, 25, "Level  2", {
+      .text(10, 25, "Level  3", {
         fontSize: "30px",
         fontStyle: "bold",
       })
@@ -53,10 +50,9 @@ export default class Level2 extends Phaser.Scene {
     this.physics.world.setBounds(0, 0, levelWidth, levelHeight);
 
     // Configurer le sol
-    this.dirtGroup = new DirtGroup(this);
-    this.grassGroup = new GrassGroup(this);
-    this.dirtGroup.addTiles(0, 9, 100, 1); // Sol d'herbe (col 0-99, ligne 9)
-    this.grassGroup.addTiles(0, 8, 100, 1); // Sol de terre (col 0-99, ligne 8)
+    this.stoneGroup = new StoneGroup(this);
+    this.stoneGroup.addTiles(0, 9, 100, 1); // Sol d'herbe (col 0-99, ligne 9)
+    this.stoneGroup.addTiles(0, 8, 100, 1); // Sol de terre (col 0-99, ligne 8)
 
     // Configurer la caméra
     this.cameras.main.setBounds(0, 0, levelWidth, levelHeight); // Limites de la caméra
@@ -64,19 +60,11 @@ export default class Level2 extends Phaser.Scene {
 
     // Ajouter un joueur
     this.player = new Pirate(this, 1, 4); // Position initiale : (1, 4), ajusté pour être sur la plateforme
-    this.physics.add.collider(this.player, this.grassGroup); // Gestion des collisions
+    this.physics.add.collider(this.player, this.stoneGroup); // Gestion des collisions
 
     // Ajouter un poulet
     this.chicken = new Chicken(this, 10, 4); // Position initiale : (1, 10), ajusté pour être sur la plateforme
-    this.physics.add.collider(this.chicken, this.grassGroup); // Gestion des collisions
-
-    this.physics.add.overlap(
-      this.player,
-      this.chicken,
-      this.nextLevel,
-      null,
-      this,
-    );
+    this.physics.add.collider(this.chicken, this.stoneGroup); // Gestion des collisions
 
     // Faire suivre la caméra au joueur
     this.cameras.main.startFollow(this.player);
@@ -94,11 +82,6 @@ export default class Level2 extends Phaser.Scene {
     });
 
     this.#handleInput();
-  }
-
-  nextLevel() {
-    this.scene.start("Level3");
-    this.scene.stop("Level2");
   }
 
   #handleInput() {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import config from "./config";
 import Level1 from "./Level1";
 import Level2 from "./Level2";
+import Level3 from "./Level3";
 import Title from "./Title";
 
 /**
@@ -12,4 +13,5 @@ const game = new Phaser.Game(config);
 game.scene.add("Title", Title, true);
 game.scene.add("Level1", Level1);
 game.scene.add("Level2", Level2);
+game.scene.add("Level3", Level3);
 
